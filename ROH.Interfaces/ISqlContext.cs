e4685ec09e0a252d795e5f31601f96a35ae68d35
Rodrigo Martins Moraes <rodrigo_moraes@hotmail.com.br>
2023-04-26 09:06:58 -0300

    SqlContext interface

---
+++ ROH.Interfaces/ISqlContext.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+using ROH.Domain.Accounts;
+using ROH.Domain.Characters;
+using ROH.Domain.Guilds;
+using ROH.Domain.Itens;
+using ROH.Domain.Kingdoms;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ROH.Interfaces
+{
+    public interface ISqlContext
+    {
+        DbSet<User> Users { get; }
+        DbSet<Account> Accounts { get; }
+        DbSet<AttackStatus> AttackStatuses { get; }
+        DbSet<Character> Characters { get; }
+        DbSet<DefenseStatus> DefenseStatuses { get; }
+        DbSet<EquipedItens> EquipedItens { get; }
+        DbSet<Skill> Skills { get; }
+        DbSet<Status> Statuses { get; }
+        DbSet<Guild> Guilds { get; }
+        DbSet<MembersPosition> MembersPositions { get; }
+        DbSet<Enchantment> Enchantments { get; }
+        DbSet<Item> Items { get; }
+        DbSet<Kingdom> Kingdoms { get; }
+        DbSet<KingdomRelation> KingdomRelations { get; }
+
+        int SaveChanges();
+        Task<int> SaveChangesAsync();
+    }
+}
