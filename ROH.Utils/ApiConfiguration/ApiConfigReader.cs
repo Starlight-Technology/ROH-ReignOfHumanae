0741cf92e99294f06d71bf6542a477ed192dedce
Rodrigo Martins Moraes <rodrigo_moraes@hotmail.com.br>
2023-08-24 08:45:42 -0300

    Utils Api Sonar linked

---
+++ ROH.Utils/ApiConfiguration/ApiConfigReader.cs
@@ -0,0 +1,39 @@
+﻿// Ignore Spelling: Utils
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Linq;
+
+namespace ROH.Utils.ApiConfiguration
+{
+    public class ApiConfigReader
+    {
+        private readonly XDocument _config;
+
+        public enum ApiUrl
+        {
+            Version
+        }
+
+        public ApiConfigReader()
+        {
+            _config = XDocument.Load("~/api-config.xml");
+        }
+
+        public Dictionary<ApiUrl, Uri> GetApiUrl()
+        {
+            var apiUrls = new Dictionary<ApiUrl, Uri>();
+
+            foreach (var serviceElement in _config.Descendants("Service"))
+            {
+                var serviceName = Enum.Parse<ApiUrl>(serviceElement.Attribute("name").Value);
+                var serviceUrl = new Uri(serviceElement.Attribute("url").Value);
+
+                apiUrls.Add(serviceName, serviceUrl);
+            }
+
+            return apiUrls;
+        }
+    }
+}
---
+++ ROH.Utils/ApiConfiguration/ApiParameters.cs
@@ -0,0 +1,16 @@
+﻿// Ignore Spelling: Api Utils
+
+namespace ROH.Utils.ApiConfiguration
+{
+    public class ApiParameters
+    {
+        public string Name { get; set; }
+        public string Value { get; set; }
+
+        public ApiParameters()
+        {
+            Name = "";
+            Value = "";
+        }
+    }
+}
