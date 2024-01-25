4717c859e719558f0de71032b83a4f1608962fe3
Rodrigo Martins Moraes <rodrigo_moraes@hotmail.com.br>
2024-01-24 21:17:06 -0300

    list component

---
+++ ROH.Blazor.Server/Helpers/Extensions/GameVersionExtensions.cs
@@ -0,0 +1,12 @@
+﻿using ROH.StandardModels.Version;
+
+namespace ROH.Blazor.Server.Helpers.Extensions
+{
+    public static class GameVersionExtensions
+    {
+        public static List<GameVersionListModel> ToListModel(this List<GameVersionModel> gameVersions)
+        {
+            return gameVersions.Select(version => version.ToListModel()).ToList();
+        }
+    }
+}
--- ROH.StandardModels/Version/GameVersionModel.cs
+++ ROH.StandardModels/Version/GameVersionModel.cs
@@ -15,5 +15,27 @@ namespace ROH.StandardModels.Version
         public GameVersionModel()
         {
         }
+
+        public GameVersionListModel ToListModel() => new GameVersionListModel(Version, Release, Review, ReleaseDate, VersionDate, Guid);
+    }
+
+    public class GameVersionListModel
+    {
+        public int Version { get; set; }
+        public int Release { get; set; }
+        public int Review { get; set; }
+        public DateTime? ReleaseDate { get; set; }
+        public DateTime VersionDate { get; set; }
+        public string DetailsLink { get; set; }
+
+        public GameVersionListModel(int version, int release, int review, DateTime? releaseDate, DateTime versionDate, Guid guid)
+        {
+            Version = version;
+            Release = release;
+            Review = review;
+            ReleaseDate = releaseDate;
+            VersionDate = versionDate;
+            DetailsLink = $"/Manager/Version/VersionDetails/{guid}";
+        }
     }
 }
