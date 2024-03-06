bcbaf95580b6d527555b4027bc78399ab9f32c56
Rodrigo Martins Moraes <rodrigo_moraes@hotmail.com.br>
2024-03-06 16:13:00 -0300

    idk why not in git

---
+++ ROH.Context/TypeConfiguration/Logs/LogTypeConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+using ROH.Domain.Logging;
+
+namespace ROH.Context.TypeConfiguration.Logs;
+public class LogTypeConfiguration : IEntityTypeConfiguration<Log>
+{
+    public void Configure(EntityTypeBuilder<Log> builder)
+    {
+        builder.HasKey(x => x.Id);
+    }
+}
