aa8bd32e04ac79bffcc1ea848293f0bf381a336a
Rodrigo Martins Moraes <rodrigo_moraes@hotmail.com.br>
2024-07-26 19:55:09 -0300

    changed some components and create login page and account page

---
+++ ROH.Blazor.Server/Interfaces/Api/IAccountService.cs
@@ -0,0 +1,15 @@
+﻿using ROH.StandardModels.Account;
+using ROH.StandardModels.Response;
+
+namespace ROH.Blazor.Server.Interfaces.Api;
+
+public interface IAccountService
+{
+    Task<DefaultResponse?> Login(LoginModel login);
+    Task<DefaultResponse?> CreateNewUser(UserModel user);
+    Task<DefaultResponse?> FindUserByEmail(string email);
+    Task<DefaultResponse?> FindUserByUserName(string userName);
+    Task<DefaultResponse?> GetUserByGuid(Guid guid);
+    Task<DefaultResponse?> GetAccountByUserGuid(Guid userGuid);
+    Task<DefaultResponse?> UpdateAccount(AccountModel account);
+}
